3dfe93d880d5a0090cf5e40fe5aebec1fd3ab7a7
https://github.com/Intannny12/IntannyUTS-elektronik.git <123intannury12@gmail.com>
2023-05-25 13:20:07 +0700

    Praktikum Pertemuan9

---
+++ app/Http/Controllers/PasienController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pasien;
+use Illuminate\Http\Request;
+
+class PasienController extends Controller
+{
+    public function index(){
+       $pasiens = Pasien::getAll();
+        return view('pasien.index', [
+            'pasiens' =>$pasiens,
+        ]);
+    }
+
+    public function create(){
+        return view('pasien.create');
+    }
+}
