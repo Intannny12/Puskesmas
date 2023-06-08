a3dcce89734634ccb887220dc0adfb9635a3a22f
https://github.com/Intannny12/IntannyUTS-elektronik.git <123intannury12@gmail.com>
2023-06-08 15:43:14 +0700

    Praktikum Pertemuan 12

--- app/Http/Controllers/PasienController.php
+++ app/Http/Controllers/PasienController.php
@@ -26,14 +26,70 @@ class PasienController extends Controller
     //method untuk menyimpan data ke database
     public function store(Request $request)
     {
+        // Validasi data
+        $request->validate([
+            'nama' => 'required|min:3',
+            'jk' => 'required',
+            'tgl_lahir' => 'required|date',
+            'alamat' => 'required',
+            'telp' => 'required|numeric',
+
+        ]);
+
+        // Simpan data hasil form ke tabel pasiens
         Pasien::create([
             'nama' => $request->nama,
             'jk' => $request->jk,
-            'tgl_lahir' =>$request->tgl_lahir,
-            'alamat'=>$request->alamat,
-            'telp'=>$request->telp,
+            'tgl_lahir' => $request->tgl_lahir,
+            'alamat' => $request->alamat,
+            'telp' => $request->telp,
         ]);
 
         return redirect('/pasien');
+    }
+    
+    // Method untuk menampilkan halaman edit
+    public function edit($id)
+    {
+        // cari pasien berdasasrkan id
+        $pasien = Pasien::find($id);
+            
+        return view('admin.pasien.edit', [
+                'pasien' => $pasien,
+        ]);
+
+    }
+
+    // Method untuk menyimpan hasil edit
+    public function update($id, Request $request){
+        // Validasi data
+        $request->validate([
+            'nama' => 'required|min:3',
+            'jk' => 'required',
+            'tgl_lahir' => 'required|date',
+            'alamat' => 'required',
+            'telp' => 'required|numeric',
+        ]);
+
+        // cari pasien berdasasrkan id
+        $pasien = Pasien::find($id);
+
+        // Simpan hasil edit
+        $pasien->update([
+            'nama' => $request->nama,
+            'jk' => $request->jk,
+            'tgl_lahir' => $request->tgl_lahir,
+            'alamat' => $request->alamat,
+            'telp' => $request->telp,
+        ]);
+
+        return redirect('/pasien')->with('success', 'Data pasien berhasil diubah.');
+    }
+
+    // method untuk menghapus data
+    public function destroy(Request $request){
+        Pasien::destroy($request->id);
+
+        return redirect('/pasien')->with('success', 'Data pasien berhasil dihapus.');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,20 @@ Route::get ('/pasien',[PasienController::class,'index' ]);
 Route::get('/pasien/create', [PasienController::class, 'create']);
 
 Route::post('/pasien',[PasienController::class, 'store']);
+
 //Route untuk menampilkan  daftar dokter
 Route::get ('/dokter',[DokterController::class, 'index']);
 
 Route::get('/dokter/create',[DokterController::class, 'create']);
 
 Route::post('/dokter', [DokterController::class, 'store']);
+
+//Route untuk menampilkan form edit pasien
+Route::get('/pasien/edit/{id}', [PasienController::class, 'edit']);
+
+//Route  untuk memproses update pasien
+Route::put('/pasien/{id}', [PasienController::class, 'update']);
+
+
+//Route untuk menghapus pasien
+Route::delete('/pasien', [PasienController::class, 'destroy']);
